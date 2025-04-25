75e119db831dee839b1e5fe7c26282c09f4c0295
guimmie <pguimera@gmail.com>
2025-04-25 16:43:30 +0200

    Fix dependencies issues on install

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,17 @@ module.exports = {
   context: __dirname,
   devtool: 'source-map',
   module: {
-    loaders: [
+    rules: [
       {
         test: /jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-2'],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            targets: "defaults",
+            presets:
+              ['@babel/preset-env', '@babel/preset-react']
+          }
         }
       }
     ]
